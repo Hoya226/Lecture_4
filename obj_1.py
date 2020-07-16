bd6f892a45bafc1acb1eeb1adaec3a61bdf3f71a
Hoya <sangho6022@gmail.com>
2020-07-16 17:57:32 +0900

    class step3

---
+++ obj_1.py
@@ -0,0 +1,13 @@
+class cat:
+    color = 'red'
+    def meow(self):
+        print('야옹~ 야옹~')
+
+
+raon = cat()
+nabi = cat()
+tong = cat()
+
+raon.meow()
+nabi.meow()
+tong.meow()
---
+++ obj_2.py
@@ -0,0 +1,32 @@
+#class cat:
+    
+   # color = 'red'
+   
+    
+    #생성자 메소드
+   # def __init__(self, name, color):
+    #    self.name = name
+     #   self.color = color
+
+
+    #def meow(self):
+     #   print("우리 {}이는 {}색깔이고 자주 야옹~ 야옹~ 해요".format(self, name, self, color))
+
+
+#raon = cat('라온', "똥")
+#raon.meow()
+
+
+class Cat:
+    color = 'red'
+    #생성자 메소드
+    def __init__(self , name , color):
+        self.name = name
+        self.color = color
+    def meow(self):
+        print("우리 {}이는 색깔이 {}구요 자주 야옹~! 야옹~! 거려요".format(self.name, self.color))
+raon = Cat('라온', "똥")
+meon = Cat('미용', '컬러플하')
+raon.meow()
+print(meon.color)
+meon.meow()
